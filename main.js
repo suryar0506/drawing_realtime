3a1795b79b0a2293dcb509745f2adc3fa03a8048
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-06-26 17:24:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("hello");
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',getPoses);
+}
+
+function draw(){
+    background('#969A97');
+}
+
+function modelLoaded(){
+    console.log("The model is ready to roll!");
+}
+
+function getPoses(results){
+    if(results.length > 0){
+console.log(results);
+    }
+}

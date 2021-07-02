2ef2dde8f1a308a8862c7df4c2388a05596ed832
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-07-02 08:55:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var noseX = 0;
+var noseY = 0;
+
+var leftWristX = 0;
+var rightWristX = 0;
+var difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,6 +18,9 @@ function setup(){
 
 function draw(){
     background('#969A97');
+    fill('#FF69B4');
+    stroke('#FF69b4');
+    square(noseX, noseY, difference);
 }
 
 function modelLoaded(){
@@ -20,5 +30,14 @@ function modelLoaded(){
 function getPoses(results){
     if(results.length > 0){
 console.log(results);
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+console.log("Nose X = " + noseX + " Nose Y = " + noseY);
+
+leftWristX = results[0].pose.leftWrist.x;
+rightWristX = results[0].pose.rightWrist.x;
+difference = floor(leftWristX - rightWristX);
+console.log("Left Wrist X = " + leftWristX + " Right Wrist X = " + rightWristX + "Difference between the 2 = " + difference);
+document.getElementById("width_display").innerHTML = difference;
     }
 }
